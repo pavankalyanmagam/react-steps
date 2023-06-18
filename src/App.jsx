b09bafd01dbb57bbef68f0c7bdb3cff8157dc175
Pavankalyan <pavankalyanmagam@gmail.com>
2023-06-18 12:40:32 -0500

    Added Steps.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,12 @@
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
+import Steps from "./components/Steps";
+
 
 function App() {
-  const handlePrevious = () => {
-    alert("Previous");
-  };
+  
 
-  const handleNext = () => {
-    alert("Next");
-  };
-
-  const step = 3;
   return (
     <>
-      <div className="steps">
-        <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-        </div>
-        <p className="message">
-          Step {step}: {messages[step - 1]}
-        </p>
-
-        <div className="buttons">
-          <button
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            onClick={handlePrevious}
-          >
-            Previous
-          </button>
-          <button
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            onClick={handleNext}
-          >
-            Next
-          </button>
-        </div>
-      </div>
+      <Steps />
     </>
   );
 }
